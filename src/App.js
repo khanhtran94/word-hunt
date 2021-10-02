ead26c8e252678a9452b3515cc0ebfc0b233dc4d
khanhTran <khanhtrantung190894@gmail.com>
2021-10-03 00:22:28 +0700

    add env file diasble esling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      Hello word
-    </div>
-  );
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/plan");
+      setMeanings(data.data);
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+  console.log(meanings);
+
+  useEffect(() => {
+    dictionaryApi();
+
+  }, []);
+
+  return <div className="App">Diationary</div>
 }
 
 export default App;
